7e0a0ffce4760f2de02af7a23f03d6e38d28a5fd
tatendabakozw <tatendabakozw@gmail.com>
2021-10-03 00:28:31 +0200

    feat: added chakra ui

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { ChakraProvider } from "@chakra-ui/react"
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ChakraProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ChakraProvider>
   </Provider>,
   document.getElementById('root')
 );
