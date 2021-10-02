3a87d06f07da8959734d8b255eb5b2f9d90d020d
tatendabakozw <tatendabakozw@gmail.com>
2021-10-03 00:17:51 +0200

    fix: added store to redux

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
+import store from './redux/store';
 
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
