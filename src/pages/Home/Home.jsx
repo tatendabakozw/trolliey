fe485cccfc14248e265cbe4a41254947293380a8
tatendabakozw <tatendabakozw@gmail.com>
2021-10-13 03:57:58 +0200

    feat: footer added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -68,7 +68,7 @@ function Home() {
 
                  {/* //two exclusive categories */}
                  <div className="grid md:grid-cols-3 grid-cols-1 md:gap-8 gap-4 md:pt-16 pt-8">
-                    <div className="col-span-2 bg-blue-200 rounded md:h-40 h-32"></div>
+                    <div className="md:col-span-2 col-start-1 bg-blue-200 rounded md:h-40 h-32"></div>
                     <div className="col-span-1 bg-yellow-200 rounded md:h-40 h-32"></div>
                 </div>
 
@@ -96,7 +96,7 @@ function Home() {
                 {/* //two Latest categories */}
                 <div className="grid md:grid-cols-3 grid-cols-1 md:gap-8 gap-4 md:pt-16 pt-8">
                     <div className="col-span-1 bg-blue-200 rounded md:h-40 h-32"></div>
-                    <div className="col-span-2 bg-yellow-200 rounded md:h-40 h-32"></div>
+                    <div className="md:col-span-2 col-start-1 bg-yellow-200 rounded md:h-40 h-32"></div>
                 </div>
 
                  {/* // featured products */}
