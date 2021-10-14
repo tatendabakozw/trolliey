31a924b461b623d3ea9ff969b614dd54a0e485ae
tatendabakozw <tatendabakozw@gmail.com>
2021-10-14 03:31:09 +0200

    feat: added not found page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import ProductDescription from './pages/product_description/ProductDescription';
 import Contact from './pages/Contact/Contact'
+import NotFound from './pages/NotFound/NotFound'
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path='/product/description/:id' component={ProductDescription} />
-        <Route exact path='/contact' component={Contact} />
+        <Route path='/contact' component={Contact} />
         <Route exact path='/' component={Home} />
+        <Route path='*' component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
