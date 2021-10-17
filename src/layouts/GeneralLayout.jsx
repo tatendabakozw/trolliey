1ae4d1ecce1ae18e7b2fc91cc6e36320be5ba84a
tatendabakozw <tatendabakozw@gmail.com>
2021-10-17 13:31:04 +0200

    fix: new navbar text padding

--- src/layouts/GeneralLayout.jsx
+++ src/layouts/GeneralLayout.jsx
@@ -10,7 +10,7 @@ function GeneralLayout({children, no_text}) {
             </div>
             <div className="lg:px-32 md:px-16 px-2 bg-gray-100 min-h-screen w-full pb-8">
                 {
-                    !no_text && (<p className="flex text-gray-700 text-sm py-2 font-semibold lg:px-8 md:px-4 px-2">Free Shipping On All Order Over $100 Code</p>) 
+                    !no_text && (<p className="flex text-gray-700 text-sm py-4 font-semibold lg:px-8 md:px-4 px-2">Free Shipping On All Order Over $100 Code</p>) 
                 }
                 {children}
             </div>
