60499191d29f7c4ac78888d7aab9605372170fe2
tatendabakozw <tatendabakozw@gmail.com>
2021-10-18 06:23:42 +0200

    feat: added logo on login and register page

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import BlueButton from '../../components/buttons/BlueButton'
 import GeneralLayout from '../../layouts/GeneralLayout'
 import { EyeIcon, EyeOffIcon } from '@heroicons/react/outline'
+import logo from '../../assets/logo.png'
 
 function Register() {
 
@@ -18,10 +19,10 @@ function Register() {
 
     return (
         <GeneralLayout no_text>
-            <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-4 sm:px-6 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                    <p className="text-center text-lg text-gray-700 font-semibold">Logo</p>
-                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Register your account</h2>
+                <img src={logo} alt="login page indicator of website" className="mx-auto self-center h-32" />
+                    <h2 className="mt-2 text-center text-3xl font-extrabold text-gray-900">Register your account</h2>
                 </div>
 
                 <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
